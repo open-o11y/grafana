0a428233ed92cd508b829a2ac11f6002b554b912
eunice98k <kimeuni@amazon.com>
2021-07-09 13:58:28 -0700

    Remove worldmapbehavior

--- public/app/plugins/panel/geomap/layers/data/index.ts
+++ public/app/plugins/panel/geomap/layers/data/index.ts
@@ -1,14 +1,12 @@
+import { circlesLayer } from './circlesOverlay';
 import { geojsonMapper } from './geojsonMapper';
 import { lastPointTracker } from './lastPointTracker';
-import { worldmapBehaviorLayer } from './worldmapBehavior';
-import { circlesLayer } from './circlesOverlay';
 
 /**
  * Registry for layer handlers
  */
 export const dataLayers = [
     circlesLayer,
-    worldmapBehaviorLayer, // mimic the existing worldmap
     lastPointTracker,
     geojsonMapper, // dummy for now
 ];
